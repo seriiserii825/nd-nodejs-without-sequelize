0efbad64ffc617560ef706f644a3ad2447026183
serii <seriiburduja@gmail.com>
2022-07-18 08:30:13 +0300

    feat: update

--- server/routes.js
+++ server/routes.js
@@ -1,48 +1,66 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("./models/user.model");
+const User = require('./models/user.model');
 
-router.get("/", (req,res) => {
-  return res.json({
-    status: 1,
-    message: "Hello World",
-  });
+router.get('/', (req, res) => {
+	return res.json({
+		status: 1,
+		message: 'Hello World',
+	});
 });
 
-router.get("/users", (req,res) => {
-  User.findAll({
-    where: {
-      status: "1"
-    }
-  }).then((users) => {
-    return res.json({
-      status: 1,
-      message: "Users fetched successfully",
-      users: users,
-    });
-  }).catch((err) => {
-    return res.json({
-      status: 0,
-      message: "Error fetching users",
-      error: err,
-    });
-  });
+router.get('/users', (req, res) => {
+	User.findAll()
+		.then((users) => {
+			return res.json({
+				status: 1,
+				message: 'Users fetched successfully',
+				users: users,
+			});
+		})
+		.catch((err) => {
+			return res.json({
+				status: 0,
+				message: 'Error fetching users',
+				error: err,
+			});
+		});
 });
 
-router.post("/user", (req, res) => {
-  User.create(req.body)
-    .then(() => {
-      return res.status(200).json({
-        status: 1,
-        message: "User created successfully",
-      });
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        status: 0,
-        message: "Error while creating user:" + err,
-      });
-    });
+router.post('/user', (req, res) => {
+	User.create(req.body)
+		.then(() => {
+			return res.status(200).json({
+				status: 1,
+				message: 'User created successfully',
+			});
+		})
+		.catch((err) => {
+			return res.status(500).json({
+				status: 0,
+				message: 'Error while creating user:' + err,
+			});
+		});
+});
+
+router.put('/update-user', (req, res) => {
+	User.update(req.body, {
+		where: {
+			id: req.body.id,
+		},
+	})
+		.then(() => {
+			return res.status(200).json({
+				status: 1,
+				message: 'User updated successfully',
+			});
+		})
+		.catch((err) => {
+			return res.status(500).json({
+				status: 0,
+				message: 'Error while updating user:' + err,
+			});
+		});
 });
 
 module.exports = router;
